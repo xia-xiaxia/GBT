7bdf3cd91aa45a948cf2d05890903c2ffd8c439a
Xiaxiaxia <3103841669@qq.com>
2024-11-24 16:22:53 +0800

    Grid

--- Assets/Scripts/PlayController/AnimComtrol.cs
+++ Assets/Scripts/PlayController/AnimComtrol.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimComtrol : MonoBehaviour
+public class PlayerFlip : MonoBehaviour
 {
-    public GameObject Player;
-    public PlayerMovement playerMove;
-    private Vector2 playerDir;
+    private PlayerMovement playerMovement;
     private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        playerDir = playerMove.moveDirection;
-        spriteRenderer = GetComponent<SpriteRenderer>();    
+        playerMovement = GetComponent<PlayerMovement>(); // 获取 PlayerMovement 脚本
+        spriteRenderer = GetComponent<SpriteRenderer>();  // 获取 SpriteRenderer 组件
     }
 
     void Update()
     {
-        if(playerDir.x > 0) 
+        // 判断玩家的移动方向
+        if (playerMovement.moveDirection.x > 0)
+        {
+            // 向右移动，确保玩家朝右
+            spriteRenderer.flipX = false;
+        }
+        else if (playerMovement.moveDirection.x < 0)
+        {
+            // 向左移动，翻转玩家精灵
             spriteRenderer.flipX = true;
-        else spriteRenderer.flipX = false;
+        }
     }
 }
+
--- Assets/Scripts/PlayController/PlayerMovement.cs
+++ Assets/Scripts/PlayController/PlayerMovement.cs
@@ -1,35 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public Vector2 moveDirection;
-    Transform transform;
+    public Vector2 moveDirection; // 玩家移动方向
+    private Vector2 targetPosition; // 目标位置
+    public float moveSpeed = 5f; // 移动速度
+    public float gridSize = 1f; // 每格的大小
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        transform = rb.transform;
+        targetPosition = transform.position; // 初始位置就是目标位置
     }
 
     void Update()
     {
+        // 获取玩家的移动输入
         float moveX = 0;
         float moveY = 0;
-        if (Input.GetKeyDown(KeyCode.W))
-            moveY++;
-        if (Input.GetKeyDown(KeyCode.S))
-            moveY--;
-        if (Input.GetKeyDown(KeyCode.A))
-            moveX--;
-        if (Input.GetKeyDown(KeyCode.D))
-            moveX++;
-        Vector2 moveDirection = new Vector2(moveX, moveY);
 
-        transform.position += new Vector3(moveX,moveY,0f);
+        if (Input.GetKey(KeyCode.W))
+            moveY++;
+        if (Input.GetKey(KeyCode.S))
+            moveY--;
+        if (Input.GetKey(KeyCode.A))
+            moveX--;
+        if (Input.GetKey(KeyCode.D))
+            moveX++;
+
+        // 设置移动方向
+        moveDirection = new Vector2(moveX, moveY).normalized; // 保证每帧的移动方向是单位向量
+
+        // 计算目标位置（下一格的坐标）
+        if (moveDirection != Vector2.zero)
+        {
+            // 计算目标位置，将玩家移动到当前格子的下一格
+            targetPosition = new Vector2(Mathf.Round(transform.position.x / gridSize) * gridSize + moveDirection.x * gridSize,
+                                         Mathf.Round(transform.position.y / gridSize) * gridSize + moveDirection.y * gridSize);
+        }
     }
 
+    void FixedUpdate()
+    {
+        // 使用插值平滑移动到目标位置
+        transform.position = Vector2.Lerp(transform.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
+    }
 }

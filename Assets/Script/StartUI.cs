e78a9ab12906b40646f8482fb20c30ac893069ca
LBZ <2751821409@qq.com>
2024-11-24 15:10:24 +0800

    dialogue

---
+++ Assets/Script/StartUI.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartUI : MonoBehaviour
+{
+    private Button startButton;
+    private Button endButton;
+
+
+
+    private void Start()
+    {
+        startButton = transform.Find("StartButton").GetComponent<Button>();
+        endButton = transform.Find("EndButton").GetComponent<Button>();
+        startButton.onClick.AddListener(OnStartButtonClicked);
+        endButton.onClick.AddListener(OnEndButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    private void OnEndButtonClicked()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/TODO.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TODO : MonoBehaviour
+{
+    //对话框大小适配不同屏幕大小
+}

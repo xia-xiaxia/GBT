77a697d53465c438790e0eb7257d755b2fab9f9d
luoluo <1463583430@qq.com>
2024-12-01 13:43:39 +0800

    Update

--- Assets/Scripts/GameConl/GameThird.cs
+++ Assets/Scripts/GameConl/GameThird.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class GameThird : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isWin;
+    public bool isFaild;
+    public EnemyFlowController enemyFlowController;
+    public GameObject gameObject;
+    public float detectionRadius;  // 圆形检测范围的半径
+    public LayerMask dreamerLayer;       // 玩家所在的层（可以通过 Inspector 设置）
+    public bool isdreamerInRange = false; // 玩家是否在检测范围内
+
     void Start()
     {
-        
+        isFaild = false;
+        isWin = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        isFaild = enemyFlowController.isGameFailed;
+        StartCoroutine(nm());
+        if (isWin) isFaild = false;
+        else if (isFaild)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void CheckKeyInRange()
+    {
+        // 使用 Physics2D.OverlapCircle 检测圆形范围内的物体
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, detectionRadius, dreamerLayer);
+
+        if (colliders.Length > 0)
+        {
+            isWin = true;
+
+        }
+        else
+        {
+            isFaild = true;
+        }
+    }
+
+    IEnumerator nm()
+    {
+        yield return new WaitForSeconds(30f);
+        CheckKeyInRange();
     }
 }
